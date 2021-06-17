b1eaa89c3b8c89adf7b3942c6b5bb258ad55f370
Profsain <profsaincrown@gmail.com>
2021-06-17 10:59:58 -0700

    Update 4-pusts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include <string.h>
+
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-  char sentence[60];
-  strcpy(sentence, "Programming is like building a multilingual puzzle\n");
-
-  puts(sentence);
-
-  return(0);
+        puts("\"Programming is like building a multilingual puzzle");
+        return(0);
 }
