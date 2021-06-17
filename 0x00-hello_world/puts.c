57e75c2b3e2a72c411c75067703c776ed757722e
Profsain <profsaincrown@gmail.com>
2021-06-17 10:49:31 -0700

    Add 4-puts.c file

---
+++ 0x00-hello_world/puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+  char sentence[60];
+  strcpy(sentence, "Programming is like building a multilingual puzzle\n");
+
+  puts(sentence);
+
+  return(0);
+}
