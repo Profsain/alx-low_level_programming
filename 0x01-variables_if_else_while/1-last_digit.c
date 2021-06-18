d22f20044c8cf60a2a70d416dd6aabbebcb74665
Profsain <profsaincrown@gmail.com>
2021-06-18 10:57:24 -0700

    1-last_digit corrected

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,12 +23,11 @@ int main(void)
 	}
 	else if ((n % 10) > 5)
 	{
-		printf("greater that 5\n");
+		printf("greater than 5\n");
 	}
 	else
 	{
-		printf("less that 6 and not 0\n");
+		printf("less than 6 and not 0\n");
 	}
-	
 	return (0);
 }
