109e66c61ace841f5f30be353d76eff67af97d70
Profsain <profsaincrown@gmail.com>
2021-06-18 12:41:01 -0700

    Add 8-print_base16.c file

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ *main - Entry point of the app
+ *print hexbase 16 in lowercase
+ *Return: Always 0 - Success
+ */
+
+int main(void)
+{
+	int n = '0';
+	int h_a_to_f = 'a';
+
+	while (n <= '9') /*print 0-9*/
+	{
+		putchar(n);
+		n++;
+	}
+
+	while (h_a_to_f <= 'f') /*print a-f*/
+	{
+		putchar(h_a_to_f);
+		h_a_to_f++
+	}
+	putchar('\n');
+
+	return (0);
+}
