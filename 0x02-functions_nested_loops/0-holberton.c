07c45e3267c646fb82a6c22e974ea85358d939ee
Profsain <profsaincrown@gmail.com>
2021-06-22 04:28:36 -0700

    Corret 0-holberton.c code

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "holberton.h"
 
 /**
  *Main function: The entry point of the app
@@ -8,7 +8,16 @@
 
 int main (void)
 {
-	printf("Holberton\n");
-
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	
 	return (0);
 }
