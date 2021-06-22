54ff6b18ebf2ff686de84cb1fd6922c7d0825215
Profsain <profsaincrown@gmail.com>
2021-06-22 11:42:02 -0700

    Add 9-times_table.c file

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ * Description: print 9 times table
+ * void: no return value
+ */
+
+void times_table(void)
+{
+	int row;
+	int column;
+	int product;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (column = 0; column <= 9; column++)
+		{
+			product = (row * column);
+
+			if (column == 0)
+			{
+				_putchar('0' + product);
+			}
+			else if (product <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + product);
+			}
+			else if (product > 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + (product / 10));
+				_putchar('0' + (product % 10));
+			}
+		}
+		_putchar('\n');
+	}
+}
