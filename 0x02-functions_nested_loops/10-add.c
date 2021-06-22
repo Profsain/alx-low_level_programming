e1edfabd52d2349fd72b9e51d7ac0e39a809f400
Profsain <profsaincrown@gmail.com>
2021-06-22 11:48:50 -0700

    Add 10-add.c file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - adds two integers
+ * @x: two integer arguments
+ * @y: two integer argument
+ * return sum of x + y
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
