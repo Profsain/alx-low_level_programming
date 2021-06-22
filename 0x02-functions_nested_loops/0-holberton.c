05b2cbd71a96f51383df5b7e2d615ee563be24b9
Profsain <profsaincrown@gmail.com>
2021-06-22 04:07:10 -0700

    Add 0-holberton.c file

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *Main function: The entry point of the app
+ *Return 0 (Success)
+ *Program to print Holberton, followed by a new line
+ */
+
+int main (void)
+{
+	printf("Holberton\n");
+
+	return (0);
+}
