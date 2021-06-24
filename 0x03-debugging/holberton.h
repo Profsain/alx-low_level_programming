dd32843bc774eaff7211901f7611da428eaf81be
Profsain <profsaincrown@gmail.com>
2021-06-24 04:08:41 -0700

    Add holberton.h file

---
+++ 0x03-debugging/holberton.h
@@ -0,0 +1,10 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/**
+ * largest_number - prints the largest of 3 integers
+ * @abc: integers to prints the largest
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c,);
