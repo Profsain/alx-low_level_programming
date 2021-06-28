5ee1eca4299b6dfc5ee2014ea1b9b851304a37ca
Profsain <profsaincrown@gmail.com>
2021-06-28 06:27:25 -0700

    Add 0-rest_to_98.c file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as parament
+ * and update the value
+ @n: pointer to int n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,22 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and error is set appropriately.
+ */
+
+int _putchar(char c);
+
+/**
+ * reset_to_98 - takes a pointer parameter and updates the vlue it point to
+ * Return: 0 on success
+ */
+
+void reset_to_98(int *n);
+
+
+#endif
