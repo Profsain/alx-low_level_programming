e85e8be9ac677da5c6fdddff7fa01188fdd219cd
Profsain <profsaincrown@gmail.com>
2021-07-05 11:36:59 -0700

    First: commit README file and holberton.h file

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,5 @@
+## ALX Holberton Software Engineering
+
+### Project file Discrition
+
+0-memset.c : A function that fills memory with a constant byte
---
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -0,0 +1,23 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/**
+ * _putchar - puts charater to standard output
+ * @c: character to put to standard output
+ */
+
+int _putchar(char c);
+
+/**
+ * *_memset - function that fills memory with a constant byte
+ * @s: memory address poited to
+ * @b: character to be filled  to the memory 
+ * @n: number of memory space to be filled
+ */
+
+char _memset(char *s, char b, unsigned int n);
+
+
+
+
+#endif
